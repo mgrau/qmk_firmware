6f825f8d9b3fa6bf0c7cdca837cc8cb5fbd883a0
Matt Grau <matt.grau@gmail.com>
2019-02-20 11:28:31 +0100

    change gh60 to be more like alt

--- keyboards/satan/keymaps/graum/keymap.c
+++ keyboards/satan/keymaps/graum/keymap.c
@@ -17,6 +17,7 @@
 
 #define _______ KC_TRNS
 #define CTL_ESC MT(MOD_LCTL,KC_ESC)
+#define CTL_GRV LCTL(KC_GRV)
 
 #define X_ALPHA UC(0x03B1)
 #define X_BETA  UC(0x03B2)
@@ -58,11 +59,11 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
    * ,-----------------------------------------------------------.
-   * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
+   * |Esc | 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
    * |-----------------------------------------------------------|
    * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |
    * |-----------------------------------------------------------|
-   * |Esc    |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
+   * |Ctl/Esc|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
    * |-----------------------------------------------------------|
    * |ShiftSC |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|ShiftSC   |
    * |-----------------------------------------------------------|
@@ -70,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
 [_BL] = LAYOUT_60_ansi(
-  KC_GESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
+  KC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS, \
   CTL_ESC, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,  \
   KC_LSPO,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         KC_RSPC, \
@@ -110,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
 [_F2] = LAYOUT_60_ansi(
-  _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,RESET,  \
+  _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,RESET,  \
   _______,_______,KC_MS_U,_______,_______,_______,_______,_______,_______,_______,_______, BL_DEC, BL_INC,BL_TOGG, \
   _______,KC_MS_L,KC_MS_D,KC_MS_R,_______,_______,_______,_______,_______,_______,_______,_______,        _______, \
   KC_BTN2,        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______, \
@@ -120,9 +121,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------------.
    * |  `|   |   |   |   |   |   |   |   |   |   |   |   |    DEL|
    * |-----------------------------------------------------------|
-   * |     |PRE|PLY|FWD|   |   |   |HOM|PUP|PDN|END|   |   |     |
+   * |     |PRE|PLY|FWD|   |   |   |HOM|PDN|PUP|END|   |   |     |
    * |-----------------------------------------------------------|
-   * |      |VL+|MUT|VL-|   |   |<- | ↑ | ↓ | ->|   |   |        |
+   * |      |VL+|MUT|VL-|   |   |<- | ↓ | ↑ | ->|   |   |        |
    * |-----------------------------------------------------------|
    * |        |   |   |   |   |   |   |   |   |   |   |          |
    * |-----------------------------------------------------------|
@@ -130,8 +131,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
 [_SL] = LAYOUT_60_ansi(
-  KC_GRV, _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_DEL ,  \
-  _______,KC_MRWD,KC_MPLY,KC_MFFD,_______,_______,_______,KC_HOME,KC_PGUP,KC_PGDN,KC_END,_______,_______,_______, \
+  CTL_GRV,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_DEL ,  \
+  _______,KC_MRWD,KC_MPLY,KC_MFFD,_______,_______,_______,KC_HOME,KC_PGDN,KC_PGUP,KC_END ,_______,_______,_______, \
   _______,KC_VOLD,KC_MUTE,KC_VOLU,_______,_______,KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,_______,_______,        _______, \
   _______,        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______, \
   _______,_______,_______,                _______,                        _______,_______,_______,_______),
