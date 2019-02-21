309367d07158663245ca655935b380845e697b47
Matt Grau <matt.grau@gmail.com>
2019-02-21 19:17:12 +0100

    add tap dance and space cadelt to alt

--- keyboards/massdrop/alt/keymaps/graum/keymap.c
+++ keyboards/massdrop/alt/keymaps/graum/keymap.c
@@ -33,20 +33,32 @@ enum alt_keycodes {
 #define _______ KC_TRNS
 #define CTL_ESC MT(MOD_LCTL, KC_ESC)
 #define CTL_GRV LCTL(KC_GRV)
+#define GRVTESC TD(TD_GRV_ESC)
+
+//Tap Dance Declarations
+enum {
+  TD_GRV_ESC = 0
+};
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for `, twice for Esc
+  [TD_GRV_ESC]  = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_ESC)
+};
 
 keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
+        GRVTESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
         CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
-        KC_LCTL, KC_LGUI, KC_LALT,                            LT(_SL, KC_SPC),                             KC_RALT, MO(_FL), KC_LEFT, KC_DOWN, KC_RGHT  \
+        KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,          KC_UP,   KC_PGDN, \
+        KC_LCTL, KC_LGUI, KC_LALT,                            LT(_SL, KC_SPC),                    KC_RALT, MO(_FL), KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [_SL] = LAYOUT(
         CTL_GRV, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-        _______, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  _______, _______, _______, _______, _______, \
+        _______, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN,  KC_END, _______, _______, _______, _______, \
         _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
         _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______  \
