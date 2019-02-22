1cc0f5b09c0aad0f9bba8c4a31aedce7ebf126b8
Matt Grau <matt.grau@gmail.com>
2019-02-22 12:21:34 +0100

    add tap dance esc to gh60

--- keyboards/satan/keymaps/graum/config.h
+++ keyboards/satan/keymaps/graum/config.h
@@ -1,1 +1,4 @@
 #define TAPPING_TOGGLE 2 // change tap toggle to only 2 taps
+
+/* Tap Dance timeout */
+#define TAPPING_TERM 150
--- keyboards/satan/keymaps/graum/keymap.c
+++ keyboards/satan/keymaps/graum/keymap.c
@@ -18,6 +18,7 @@
 #define _______ KC_TRNS
 #define CTL_ESC MT(MOD_LCTL,KC_ESC)
 #define CTL_GRV LCTL(KC_GRV)
+#define GRVTESC TD(TD_GRV_ESC)
 
 #define X_ALPHA UC(0x03B1)
 #define X_BETA  UC(0x03B2)
@@ -56,6 +57,18 @@
 #define X_SUP9  UC(0x2079)
 
 #define X_IBANG UC(0x203D)
+
+//Tap Dance Declarations
+enum {
+  TD_GRV_ESC = 0
+};
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for `, twice for Esc
+  [TD_GRV_ESC]  = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_ESC)
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Layer
    * ,-----------------------------------------------------------.
@@ -71,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
 [_BL] = LAYOUT_60_ansi(
-  KC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
+  GRVTESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL, KC_BSPC, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS, \
   CTL_ESC, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,  \
   KC_LSPO,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         KC_RSPC, \
@@ -112,9 +125,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_F2] = LAYOUT_60_ansi(
   _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,RESET,  \
-  _______,_______,KC_MS_U,_______,_______,_______,_______,_______,_______,_______,_______, BL_DEC, BL_INC,BL_TOGG, \
+  _______,_______,KC_MS_U,_______,_______,_______,_______,_______,_______,_______,_______,BL_DEC , BL_INC,BL_TOGG, \
   _______,KC_MS_L,KC_MS_D,KC_MS_R,_______,_______,_______,_______,_______,_______,_______,_______,        _______, \
-  KC_BTN2,        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,        _______, \
+  KC_BTN2,        _______,_______,_______,_______,RESET  ,_______,_______,_______,_______,_______,        _______, \
   _______,_______,_______,                KC_BTN1,                        _______,_______,_______,_______),
 
   /* Keymap _SL: Spapcebar Layer
