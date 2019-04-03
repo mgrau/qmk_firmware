063dd05ea7820d8dc6d384ee7077e09688be65de
Matt Grau <matt.grau@gmail.com>
2019-04-03 20:33:36 +0200

    changed control to be tap for delete

--- keyboards/dz60/keymaps/graum/keymap.c
+++ keyboards/dz60/keymaps/graum/keymap.c
@@ -8,6 +8,7 @@
 
 #define _______ KC_TRNS
 #define CTL_ESC MT(MOD_LCTL, KC_ESC)
+#define DEL_CTL MT(MOD_LCTL, KC_DEL)
 
 #define MGLAYOUT( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c,      k0e, \
@@ -44,14 +45,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,         \
     CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,          \
     KC_LSPO,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSPC,         \
-    KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,           KC_SPC,           LT(_SL, KC_SPC),  MO(_FL), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+    DEL_CTL, KC_LGUI, KC_LALT,          KC_SPC,           KC_SPC,           LT(_SL, KC_SPC),  MO(_FL), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 	[_SL] = MGLAYOUT(
     KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,          KC_DEL, \
-    _______, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN,  KC_END, _______, _______, _______,         \
+    _______, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______, _______, _______,         \
     _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,         \
     _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,         \
-    _______, _______, _______,          _______,          _______,          _______,          _______, _______, _______, _______, _______ \
+    _______, _______, _______,          _______,          _______,          _______,          _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
 ),
 	[_FL] = MGLAYOUT(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,RGB_RMOD, RGB_MOD,          RGB_TOG,\
