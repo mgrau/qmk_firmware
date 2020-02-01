5af73bbfb46969636195fe9e9ed3d8f9b2956d76
Matt Grau <matt.grau@gmail.com>
2020-02-01 03:13:21 +0100

    add cadet and greek to dz60 from alt

--- keyboards/dz60/keymaps/graum/keymap.c
+++ keyboards/dz60/keymaps/graum/keymap.c
@@ -1,21 +1,31 @@
 #include QMK_KEYBOARD_H
 
-enum mg_layers {
-  _BASE,     // base layer
-  _LOWER,    // lower layer
-  _RAISE,    // raise layer
-  _ADJUST,   // adjust tri state layer
-  _FUNCTION, // function layer
-  _GAME,     // gaming layer
+enum layers {
+    BASE = 0,   // base layer
+    FUN,        // function layer
+    GAME,       // gaming layer
+    GIT,        // git macro layer
+    CAD,        // cadet unicode layer
+    FNT,        // front cadet layer
+    UP,         // shifted front cadet layer
+    FUT,        // futhark layer
+    RAISE,
+    LOWER,
+    ADJ,        // rasie + lower
 };
 
-#define LOWER LT(_LOWER, KC_SPC)
-#define RAISE LT(_RAISE, KC_SPC)
-#define FUNCT MO(_FUNCTION)
+
 
 #define _______ KC_TRNS
 #define CTL_ESC MT(MOD_LCTL, KC_ESC)
+#define CTL_GRV LCTL(KC_GRV)
+#define GIT_GRV LT(GIT, KC_GRV)
 #define DEL_CTL MT(MOD_LCTL, KC_DEL)
+#define SPACE_MOD LT(SPACE, KC_SPC)
+#define LOW_MOD LT(LOWER, KC_SPC)
+#define RAS_MOD LT(RAISE, KC_SPC)
+#define FNT_TAB LT(FNT, KC_TAB)
+#define FNT_ENT LT(FNT, KC_ENT)
 
 #define MGLAYOUT( \
     k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0a, k0b, k0c,      k0e, \
@@ -47,42 +57,69 @@ enum mg_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	[_BASE] = MGLAYOUT(
+	[BASE] = MGLAYOUT(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,\
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,         \
-    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,          \
+    FNT_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,         \
+    CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          FNT_ENT,         \
     KC_LSPO,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSPC,         \
-    DEL_CTL, KC_LGUI, KC_LALT,          LOWER,            KC_SPC,           RAISE,            FUNCT,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
-),
-	[_RAISE] = MGLAYOUT(
+    DEL_CTL, KC_LGUI, KC_LALT,          LOW_MOD,          KC_SPC,           RAS_MOD,          MO(FUN), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+    ),
+	[RAISE] = MGLAYOUT(
     KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,          KC_DEL, \
     _______, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______, _______, _______,         \
     _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,         \
     _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,         \
     _______, _______, _______,          _______,          _______,          _______,          _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
-),
-	[_LOWER] = MGLAYOUT(
+    ),
+	[LOWER] = MGLAYOUT(
     KC_ESC,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,          KC_DEL, \
     _______, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______, _______, _______,         \
     _______, KC_VOLD, KC_MUTE, KC_VOLU, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,         \
     _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,         \
     _______, _______, _______,          _______,          _______,          _______,          _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END  \
-),
-	[_ADJUST] = MGLAYOUT( 
+    ),
+	[ADJUST] = MGLAYOUT(
     _______, KC_A   , _______, _______, _______, _______, _______, _______, _______, _______, _______,RGB_RMOD, RGB_MOD,          RGB_TOG,\
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,         \
     _______,          _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______,          _______,         \
     _______, _______, _______,          _______,          _______,          _______,          _______, _______, _______, _______, _______ \
-),
-	[_FUNCTION] = MGLAYOUT(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,RGB_RMOD, RGB_MOD,          RGB_TOG,\
+    ),
+    [FUN] = MGLAYOUT(
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,          _______,\
+    _______, RGB_RMOD,RGB_TOG, RGB_MOD, _______, _______, _______, UT_AUTO, UT_AGCR, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______,         \
+    _______, _______, _______, _______, _______, TG(GAME),_______, _______, _______, _______, _______, _______,          _______,         \
+    _______,          _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______,          _______,         \
+    UC_M_LN, UC_M_WC, UC_M_OS,          _______,          _______,          _______,          _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END \
+    ),
+    [GAME] = MGLAYOUT(
+    KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,RGB_RMOD, RGB_MOD,          RGB_TOG,\
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,         \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,         \
     _______,          _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______,          _______,         \
-    _______, _______, _______,          _______,          _______,          _______,          _______, _______, _______, _______, _______ \
-  )
-
+    _______, _______, _______,          KC_SPC,           _______,          KC_SPC,           _______, _______, _______, _______, _______ \
+    ),
+    [CAD] = MGLAYOUT(
+    X(IBNG), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        , _______, \
+    _______, X(AND),  X(OR),   X(INSC), X(UNI),  X(SUB),  X(SUP),  X(FALL), X(INF),  X(EXIS), X(PART), _______, _______, _______,        , \
+    _______, X(UPTK), X(DNTK), X(RTTK), X(LTTK), X(LRAR), X(LARR), X(DARR), X(UARR), X(RARR), _______, _______,          _______,        , \
+    _______,          X(LDO),  X(LUP),  X(NOTE), X(ASYM), X(EQIV), X(LEQ),  X(GEQ),  _______, _______, _______,          _______,        , \
+    _______, _______, _______,          _______,          _______,          _______,          _______, _______, _______, _______, _______  \
+    ),
+    [FNT] = MGLAYOUT(
+    _______, X(F_1),  X(F_2),  X(F_3),  X(F_4),  X(F_5),  X(F_6),  X(F_7),  X(F_8),  X(F_9),  X(F_0),  X(F_11), X(F_12),        , _______, \
+    _______, X(F_q),  X(F_w),  X(F_e),  X(F_r),  X(F_t),  X(F_y),  X(F_u),  X(F_i),  X(F_o),  X(F_p),  _______, _______, _______,        , \
+    _______, X(F_a),  X(F_s),  X(F_d),  X(F_f),  X(F_g),  X(F_h),  X(F_j),  X(F_k),  X(F_l),  _______, _______,          _______,        , \
+    MO(UP),           X(F_z),  X(F_x),  X(F_c),  X(F_v),  X(F_b),  X(F_n),  X(F_m),  X(F_ll), X(F_gg), X(Fint),          MO(UP),         , \
+    _______, _______, _______,          _______,          _______,          _______,          _______, _______, _______, _______, _______  \
+    ),
+    [UP] = MGLAYOUT(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,        , _______, \
+    _______, X(F_Q),  X(F_W),  X(F_E),  X(F_R),  X(F_T),  X(F_Y),  X(F_U),  X(F_I),  X(F_O),  X(F_P),  _______, _______, _______,        , \
+    _______, X(F_A),  X(F_S),  X(F_D),  X(F_F),  X(F_G),  X(F_H),  X(F_J),  X(F_K),  X(F_L),  _______, _______,          _______,        , \
+    _______, X(F_Z),  X(F_X),  X(F_C),  X(F_V),  X(F_B),  X(F_N),  X(F_M),  _______, _______, _______, _______,          _______,        , \
+    _______, _______, _______,          _______,          _______,          _______,          _______, _______, _______, _______, _______  \
+    ),
 };
 
 // Runs just one time when the keyboard initializes.
